5a293979a35346715324e2a9785b4a1beda8fe06
baonguyen <bao678393@gmail.com>
2026-01-10 14:37:29 +0700

    sua

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,18 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
 
-
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
-      // Prevent node modules from being bundled
-      'events': path.resolve(__dirname, './polyfills.ts'),
-      'util': path.resolve(__dirname, './polyfills.ts'),
-      'buffer': path.resolve(__dirname, './polyfills.ts'),
-      'stream': path.resolve(__dirname, './polyfills.ts'),
+      buffer: 'buffer',
     },
   },
 
+  define: {
+    global: {},
+  },
+
   optimizeDeps: {
-    exclude: ['events', 'util', 'buffer', 'stream'],
     esbuildOptions: {
       define: {
         global: 'globalThis',
@@ -26,26 +24,16 @@ export default defineConfig({
     },
   },
 
-  worker: {
-    format: 'es',
-    rollupOptions: {
-      external: ['events', 'util', 'buffer', 'stream'],
-    },
-  },
-
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    sourcemap: false,
     commonjsOptions: {
       transformMixedEsModules: true,
     },
     rollupOptions: {
-      external: ['events'],
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
-          ui: ['lucide-react', 'recharts'],
         },
       },
     },
